0b020642308c4d210a8002e4e447a4cd8a2d25ec
Aurélien Charpilienne <aurelien.charpilienne@gmail.com>
2023-03-20 17:26:01 +0100

    refactor: freelance experience updated

--- src/data/experiences/freelance.ts
+++ src/data/experiences/freelance.ts
@@ -32,13 +32,37 @@ const Freelance = [
       <br/>
       <a target="_blank" href="https://res-artes.com/">https://res-artes.com/</a><br/>
       <br/><br/>
-      <b>💻 Monuma 💻</b><br/>
+      <b>💻 Saretec — Monuma 💻</b><br/>
       <br/>
       Refonte complète d'un panel administrateur. Monuma opère dans l'expertise, la valorisation et la traçabilité d'objets d'arts, en s'appuyant sur la technologie de la Blockchain.<br/>
       <br/>
       ➔ Développement Front-End <b>[React.js + TypeScript]</b><br/>
       <br/>
-      <a target="_blank" href="https://saretec.fr/fr/activites/conseil-prevention/monuma-0">https://saretec.fr/ — Monuma</a>
+      <a target="_blank" href="https://saretec.fr/fr/activites/conseil-prevention/monuma-0">https://saretec.fr/ — Monuma</a><br/>
+      <br/><br/>
+      <b>💻 WeDoData 💻</b><br/>
+      Développement d'une carte de la France interactive, permettant de comparer une donnée configurable entre chaque commune française, par rapport à une deuxième carte synchronisée comportant une autre donnée.<br/>
+      <br/>
+      Exemple : orientation politique par rapport à l'âge moyen, pour chaque commune française<br/>
+      <br/>
+      ➔ Développement Front-End <b>[React.js + TypeScript]</b><br/>
+      — Carte interactive et dynamique<br/>
+      — Zoom sur chaque département français afin d'accéder au détail de chaque commune<br/>
+      — Synchronisation des coordonnées de la souris avec une deuxième carte<br/>
+      ➔ Développement de Scripts <b>[Node.js + TypeScript]</b><br/>
+      — Simplification et projection d'un GeoJSON en EPSG:3857<br/>
+      — Génération d'un SVG depuis le GeoJSON, puis d'un PNG coloré en fonction des résultats<br/>
+      — Génération de fichiers de coordonnées mappées entre les codes INSEE et les pixels de l'image de la carte<br/>
+      <br/>
+      <a target="_blank" href="https://wedodata.fr/">https://wedodata.fr/</a><br/>
+      <br/><br/>
+      <b>💻 UGLA 💻</b><br/>
+      <br/>
+      Audit de performance sur un système de salle immersive pilotée par une tablette, permettant à un magasin d'offrir à ses clients une expérience d'achat dynamique grâce à plusieurs projecteurs.<br/>
+      <br/>
+      ➔ Développement Back-End <b>[PHP]</b><br/>
+      <br/>
+      <a target="_blank" href="https://ugla.tech/">https://ugla.tech/</a><br/>
     `,
   },
   {
