66450dc993ac3e907539b8580ec116d5d8be71a2
Aurélien Charpilienne <aurelien.charpilienne@gmail.com>
2023-06-11 14:53:57 +0200

    refactor: code cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "styled-components"
+import { ThemeProvider } from "styled-components";
 
 import App from "./app";
 import { theme } from "./theme";
