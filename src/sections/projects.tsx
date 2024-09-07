cdaa320e404dd143e4b28da107f4a7e94bff1038
Aurélien Charpilienne <aurelien.charpilienne@gmail.com>
2024-09-07 18:30:40 +0200

    feat: projects: title

--- src/sections/projects.tsx
+++ src/sections/projects.tsx
@@ -1,9 +1,26 @@
 import styled from "styled-components";
+import { useTranslation } from "react-i18next";
+
+import { GradientText } from "@/utils/styles";
 
 const Projects = () => {
-  return <Container></Container>;
+  const { t } = useTranslation();
+
+  return (
+    <Container>
+      <Title>{t("projects.title")}</Title>
+    </Container>
+  );
 };
 
 export default Projects;
 
-const Container = styled.div``;
+const Container = styled.div`
+  padding: 8rem;
+`;
+
+const Title = styled(GradientText)`
+  text-align: center;
+  font-weight: 700;
+  font-size: 5rem;
+`;
