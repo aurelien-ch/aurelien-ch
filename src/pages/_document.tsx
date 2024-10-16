03e4179f0c4b9c6c08b336e52c8ae1efd1d94d31
Aurélien Charpilienne <aurelien.charpilienne@gmail.com>
2024-10-16 19:14:37 +0200

    seo: .ico link rel

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,21 +8,20 @@ import Document, {
 } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 import { AppProps, AppType } from "next/app";
-
-import { APP_DESCRIPTION, APP_NAME } from "@/utils/globals";
+import translations from "../../public/locales/en/common.json";
 
 const MyDocument = (props: DocumentInitialProps) => {
   return (
     <Html lang="en">
       <Head>
         {props.styles}
-        <meta property="og:title" content={APP_NAME} />
-        <meta property="og:description" content={APP_DESCRIPTION} />
+        <meta property="og:title" content={translations._app.title} />
+        <meta property="og:description" content={translations._app.description} />
         <meta property="og:image" content="" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="" />
-        <meta name="twitter:title" content={APP_NAME} />
-        <meta name="twitter:description" content={APP_DESCRIPTION} />
+        <meta name="twitter:title" content={translations._app.title} />
+        <meta name="twitter:description" content={translations._app.description} />
         <meta name="twitter:image" content="" />
         <meta name="twitter:image:alt" content="" />
         <link rel="preload" href="/fonts/Poppins.otf" as="font" crossOrigin="" type="font/otf" />
@@ -30,6 +29,7 @@ const MyDocument = (props: DocumentInitialProps) => {
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="msapplication-TileColor" content="#ffffff" />
         <meta name="theme-color" content="#ffffff" />
