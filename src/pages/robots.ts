cb53556b88c5149b93f65192bb61f2b3f77e1ddc
Aurélien Charpilienne <aurelien.charpilienne@gmail.com>
2024-10-16 19:24:29 +0200

    seo: robots + sitemap

---
+++ src/pages/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://aurelien-ch.com/sitemap.xml',
+  }
+}
